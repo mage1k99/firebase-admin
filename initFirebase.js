ca20fa4f13446d6d1c6ab0ddb5375f7269829e0f
mage <mac@localhost.localdomain>
2019-07-16 22:05:57 +0530

    initial commit

---
+++ initFirebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCWkymQpVvfTLn6x39Y97rVf1_UPOLlPBc",
+    authDomain: "nullplay-af1234.firebaseapp.com",
+    databaseURL: "https://nullplay-af1234.firebaseio.com",
+    projectId: "nullplay-af1234",
+    storageBucket: "nullplay-af1234.appspot.com",
+    messagingSenderId: "727564140821",
+    appId: "1:727564140821:web:cae17aa69a48c025"
+  };
+  firebase.initializeApp(firebaseConfig);
